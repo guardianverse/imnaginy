b5316f090f9e4f09ffc2dc2d84ae3cd2dc500080
Guardian <guardiandelatorrot@gmail.com>
2025-10-11 23:06:28 +0200

    fix: clerk middleware y runtime nodejs 2

--- middleware.ts
+++ middleware.ts
@@ -21,16 +21,16 @@
 // };
 
 
-
-
-// middleware.ts
+// middleware.ts (o src/middleware.ts si usas /src)
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next internals y estáticos
     "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    // Siempre correr en API
     "/(api|trpc)(.*)",
   ],
 };
